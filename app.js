2ed3b237a77ad445696461574c81aed4e96f1395
ruthvikbhairav <ruthvikyalavarthi@gmail.com>
2021-10-27 21:29:02 -0500

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydata = require('./routes/mydata');
-var computes = require('./routes/computes');
+var compute = require('./routes/compute');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydata);
-app.use('/computes', computes);
+app.use('/compute', compute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
