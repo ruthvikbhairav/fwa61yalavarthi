d16383c227d6ea6c387200a97bd94b2db41daf3b
ruthvikbhairav <ruthvikyalavarthi@gmail.com>
2021-10-27 21:13:58 -0500

    added computes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydata = require('./routes/mydata');
+var computes = require('./routes/computes');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydata);
+app.use('/computes', computes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
